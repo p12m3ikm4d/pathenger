3da2e9b70f2bfbac118521492e88a3fc43065017
SongChihyun <sch@samwooim.com>
2025-01-14 15:15:02 +0900

    mod version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pathenger",
-    version="0.2.0",
+    version="0.3.0",
     author="p12m3ikm4d",
     author_email="issue.no9@gmail.com",
     description='A utility package for determining the paths of executable files and temporary directories, especially useful for applications packaged with PyInstaller.',
